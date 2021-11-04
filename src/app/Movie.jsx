bd330c7f03ebbedc3271a13cb5b9dc5cd8fc5a4d
ShunyaNagashige <nagashige190441091@gmail.com>
2021-11-04 23:38:05 +0900

    ビデオ通話用のコンテナを実装 #6

--- src/app/Movie.jsx
+++ src/app/Movie.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import { Button, Container } from "@material-ui/core";
+import { Box, Button, Container, Grid } from "@material-ui/core";
 
 const Movie = () => {
+  const [appID, setAppID] = useState('');
+  const [token, setToken] = useState('');
+  const [channel, setChannel] = useState('');
+  const {
+    localAudioTrack,
+    localVideoTrack,
+    leave,
+    join,
+    joinState,
+    remoteUsers,
+  } = useAgora(client);
+  
   return (
-    <Container justify="center" spacing={4}>
-      <h1>映画見る画面</h1>
-      <Link to="/" style={{ textDecoration: "none" }}>
-        <Button variant="outlined">トップページに戻る</Button>
-      </Link>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Grid container>
+        {remoteUsers.map(user => (
+          <Grid item xs={2}>
+            {/* <MediaPlayer
+              videoTrack={user.videoTrack}
+              audioTrack={user.audioTrack}
+            ></MediaPlayer> */}
+          </Grid>
+        ))}
+      </Grid>
+    </ThemeProvider>
+    // <Container justify="center" spacing={4}>
+    //   <h1>映画見る画面</h1>
+    //   <Link to="/" style={{ textDecoration: "none" }}>
+    //     <Button variant="outlined">トップページに戻る</Button>
+    //   </Link>
+    // </Container>
   );
 };
 
