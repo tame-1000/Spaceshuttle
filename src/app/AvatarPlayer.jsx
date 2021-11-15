1ccb9cedabf9113e3aabf63cf579fa86ace06c27
ShunyaNagashige <nagashige190441091@gmail.com>
2021-11-15 12:19:06 +0900

    とりあえず顔認識を行い，canvasにその結果を描画するように実装 #7

--- src/app/AvatarPlayer.jsx
+++ src/app/AvatarPlayer.jsx
@@ -1,23 +1,52 @@
 import { Grid, makeStyles } from "@material-ui/core";
 import React, { useEffect, useRef } from "react";
+import clm from "clmtrackr"
 
 export const AvatarPlayer = ({ video }) => {
   const useStyles = makeStyles(() => ({
     video: {
-      width: "80vh",
-      height: "auto",
+      transform: "scaleX(-1)"
     },
+    canvas: {
+      transform: "scaleX(-1)",
+    }
   }));
 
   const classes = useStyles();
 
   const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  let ctracker = null;
 
   useEffect(() => {
-    if (videoRef.current) {
+    if (videoRef.current && canvasRef.current) {
       videoRef.current.srcObject = video.stream;
-      videoRef.current.play().catch((e) => console.log(e));
+
+      ctracker = new clm.tracker();
+      ctracker.init();
+
+      let box = [0, 0, 400, 260];
+      ctracker.start(videoRef.current, box);
+
+      document.addEventListener('clmtrackrConverged', () => console.log('Converged'))
+      document.addEventListener('clmtrackrLost', () => console.log('Lost'))
+
+      let cc = canvasRef.current.getContext("2d");
+
+      function drawLoop() {
+        requestAnimationFrame(drawLoop); // ここで毎フレームdrawLoopを呼び出すように設定します。
+        cc.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height); // 毎フレーム出力用のキャンバスをクリアします。これをしないと重ね書きのようになってしまいます。
+        ctracker.draw(canvasRef.current); // 判定結果をcanvasに描画します。
+        console.log(ctracker.getCurrentPosition())
+      }
+
+      drawLoop();
     }
+
+    return () => {
+      ctracker.stop();
+    };
   }, [video]);
 
   return (
@@ -27,7 +56,11 @@ export const AvatarPlayer = ({ video }) => {
         ref={videoRef}
         playsInline
         autoPlay
+        width={300}
+        height={300}
       ></video>
+      {/* {canvas} */}
+      <canvas className={classes.canvas} ref={canvasRef} width={400} height={300}></canvas>
     </Grid>
   );
 };
