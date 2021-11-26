70e3afc2bb8fbc654c64ff1c9a53f9e7c53cce94
Neru-neru <yumadachi0113soccer@gmail.com>
2021-11-27 02:29:59 +0900

    switch whether signin/register or not

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -6,22 +6,40 @@ import Nav from "./Nav";
 
 const Layout = (props) => {
   const location = useLocation();
-  return (
-    <>
-      <AuthProvider>
-        <Nav pathname={location.pathname}></Nav>
-        <div
-          style={{
-            paddingTop: "80px",
-            backgroundColor: "#F8F8FF",
-            minHeight: "100%",
-          }}
-        >
-          {props.children}
-        </div>
-      </AuthProvider>
-    </>
-  );
+
+  if (location.pathname !== "/signin" && location.pathname !== "/register") {
+    return (
+      <>
+        <AuthProvider>
+          <Nav pathname={location.pathname}></Nav>
+          <div
+            style={{
+              paddingTop: "80px",
+              backgroundColor: "#F8F8FF",
+              minHeight: "100%",
+            }}
+          >
+            {props.children}
+          </div>
+        </AuthProvider>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <AuthProvider>
+          <Nav pathname={location.pathname}></Nav>
+          <div
+            style={{
+              height: "100%",
+            }}
+          >
+            {props.children}
+          </div>
+        </AuthProvider>
+      </>
+    );
+  }
 };
 
 export default Layout;
